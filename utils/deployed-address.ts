06eaa5476c7606190eca2bbc5e1f3c7c4772445c
root <root@DESKTOP-4400F6U>
2024-07-31 12:25:12 +0100

    feat: Completed challenge

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x610ca2e2f189aAe3e68272d6c62c603F56ABE471'
+export const ProxyAdmin = '0x1277c7E9cd22733636be8Aa96fD1e19bf7793028'
+export const SWTRImplementation = '0x99c41EDACd58aEe2007cB5f92a6244A94afB1AdA'
+    
